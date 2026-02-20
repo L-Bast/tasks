300e4c1587e999c980e36fa471f569a68585beb7
liam <liambast@gmail.com>
2026-02-20 01:16:00 -0500

    Added the changes needed for task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import catPhoto from "./catExamplePhoto.jpg";
 
 function App(): React.JSX.Element {
     return (
@@ -8,10 +10,46 @@ function App(): React.JSX.Element {
                 Hello World! Liam Bast UD CISC275 with React Hooks and
                 TypeScript
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <h1>This is needed for a test</h1>
+            Top 3 Days of the Week:
+            <ul>
+                <li>Saturday</li>
+                <li>Friday</li>
+                <li>Sunday</li>
+            </ul>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col
+                        style={{
+                            border: "1px solid red",
+                            padding: "4px",
+                            backgroundColor: "red",
+                        }}
+                    >
+                        To the right is a photo of a cat
+                    </Col>
+                    <Col
+                        style={{
+                            border: "1px solid red",
+                            padding: "4px",
+                            backgroundColor: "red",
+                        }}
+                    >
+                        <img
+                            src={catPhoto}
+                            alt="A photo of a cat"
+                            className="small-image"
+                        />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
